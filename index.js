7f143f8401fb6d440e6a63fa483155d0be8d4756
JoshMarte <joshuelm@gmail.com>
2021-12-02 16:21:12 -0500

    second

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-function fn(...c) {
-    if (!c.every((a) => Number.isNumber(a)))
-      throw "All arguments must be numbers.";
-    return c.reduce((a, b) => a + b);
-  }
-  
-  fn(1, 2, 3); //> 6
-  fn(10, "B", 20); //> error All arguments must be numbers.
+function fn(...array) {
+  if (!array.every((item) => typeof item === "number"))
+    throw "All arguments must be numbers.";
+  return array.reduce((acc, curr) => acc + curr);
+  console.log(acc, curr);
+}
+
+// fn(1, 2, 3)); //> 6
+// fn(10, "B", 20); //> error All arguments must be numbers.
