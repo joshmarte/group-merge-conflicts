2a2b73077189351b61df5dd67991d7f830390025
Mukayila ALAO <mukayilaalao@pursuit.org>
2021-12-02 16:22:06 -0500

    fn back

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-function addArray(...arr) {
+function fn(...arr) {
     if (arr.every((el) => Number.isNaN(el))) return "All arguments must be numbers.";
     return arr.reduce((a, b) => a + b);
   }
   
-  // console.log(addArray(1, 2, 3)); //> 6
-  console.log(addArray(10, "B", 20)); //> error All arguments must be numbers.
+  // console.log(fn(1, 2, 3)); //> 6
+  console.log(fn(10, "B", 20)); //> error All arguments must be numbers.
